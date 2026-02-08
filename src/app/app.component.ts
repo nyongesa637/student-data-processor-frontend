55d948bfdbbdd6f208e2690b329a9195cd667d7c
nyongesa637 <nimrodnyongesa7@gmail.com>
2026-02-09 01:36:48 +0300

    Set up app component with tab navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { DataGenerationComponent } from './components/data-generation/data-generation.component';
+import { DataProcessingComponent } from './components/data-processing/data-processing.component';
+import { DataUploadComponent } from './components/data-upload/data-upload.component';
+import { ReportComponent } from './components/report/report.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [MatTabsModule, MatToolbarModule, DataGenerationComponent, DataProcessingComponent, DataUploadComponent, ReportComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'student-data-processor-frontend';
+  title = 'Student Data Processor';
 }
